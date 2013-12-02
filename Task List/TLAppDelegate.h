2579ac96c4fe1359a5f136e97bb47738e249ecc7
Jimzy Lui <jimzylui_desktop@stickyaznrabbit.com>
2013-12-01 19:20:49 -0500

    Initial Commit

---
+++ Task List/TLAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  TLAppDelegate.h
+//  Task List
+//
+//  Created by Jimzy Lui on 12/1/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Task List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Task List
+//
+//  Created by Jimzy Lui on 12/1/2013.
+//  Copyright (c) 2013 Jimzy Lui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TLAppDelegate class]));
+    }
+}
